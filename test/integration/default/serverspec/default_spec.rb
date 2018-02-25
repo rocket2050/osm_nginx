1303b0f615212078032c1845019f2b8cdbef3198
arpit2356 <arpit2356@gmail.com>
2018-02-25 16:15:45 +0530

    modifications added

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,58 +1,27 @@
 require 'serverspec'
- 
-set :backend, :exec
- 
-nexus_version = '2.13.0-01'
- 
-['wget', 'createrepo'].each do |pkg|
-	  describe package(pkg) do
-		      it { is_expected.to be_installed }
-		        end
+require 'spec_helper'
+
+set :backend, :ssh
+
+
+describe package('nginx'), :if => os[:family] == 'ubuntu' do
+	it { should be_installed }
 end
- 
-describe group('nexus') do
-	  it { is_expected.to exist }
+
+describe package('nginx'), :if => os[:family] == 'redhat' do
+	it { should be_installed }
 end
- 
-describe user('nexus') do
-	  it { is_expected.to exist }
-	    it { is_expected.to belong_to_group 'nexus' }
-	      it { is_expected.to have_login_shell '/bin/bash' }
+
+describe service('nginx'), :if => os[:family] == 'ubuntu' do
+	it { should be_enable }
+	it { should be_running }
 end
- 
-describe file('/usr/share') do
-	  it { is_expected.to be_directory }
-	    it { is_expected.to be_owned_by 'root' }
-	      it { is_expected.to be_grouped_into 'root' }
+
+describe service('nginx'), :if => os[:family] == 'redhat' do
+	it { should be_enable }
+	it { should be_running }
 end
- 
-[
-	  '/var/nexus',
-	    '/var/run/nexus',
-	      "/usr/share/nexus-#{nexus_version}-bundle.tar.gz",
-]
-	.each do |dir|
-	  describe file(dir) do
-		      it { is_expected.to be_directory }
-		          it { is_expected.to be_owned_by 'nexus' }
-			      it { is_expected.to be_grouped_into 'nexus' }
-			        end
-end
- 
-describe file('/etc/init.d/nexus') do
-	  its(:content) { is_expected.to match /RUN_AS_USER="nexus"/ }
-end
- 
-describe file("/usr/share/nexus-#{nexus_version}/conf/nexus.properties") do
-	  its(:content) { is_expected.to match /application-host=0.0.0.0/ }
-	    its(:content) { is_expected.to match /application-port=8082/ }
-	      its(:content) { is_expected.to match /nexus-work=\/var\/nexus/ }
-end
- 
-describe service('nexus') do
-	  it { is_expected.to be_running }
-end
- 
-describe port(8082) do
-	  it { is_expected.to be_listening.with('tcp') }
+
+describe port(80) do
+	it  { should be_listening }
 end
